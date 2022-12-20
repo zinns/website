4d66f1999213972db78015874844c6a9a162e6d4
Edgar Zea <cesszea@icloud.com>
2022-12-19 19:02:06 -0600

    initial configuration

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
