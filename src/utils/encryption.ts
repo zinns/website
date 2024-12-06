aa0eea6cb60a7c065625222c78407405c2877cb3
Edgar Zea <cesszea@icloud.com>
2024-12-05 22:00:18 -0600

    feat(migration-next): install nextjs 15.0.4 (#22)

--- src/utils/encryption.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const generateSalt = async () => await bcrypt.genSalt(10);
-
-export const generateHash = async (toHash: string, salt: string) => await bcrypt.hash(toHash, salt);
-
-export const isHashCorrect = async (toHash: string, fromHash: string) =>
-  await bcrypt.compare(toHash, fromHash);
