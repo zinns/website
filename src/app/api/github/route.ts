8e1144e2fb426e0b07e7ccd7a21b2d58f775af7c
Edgar Zea <cesszea@icloud.com>
2024-12-08 15:25:22 -0600

    feat(webhook): test request

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -1,8 +1,8 @@
 import * as crypto from 'crypto';
+import { onIssue, onStar } from '@/helpers/githubEvents';
 import { NextResponse } from 'next/server';
 
 export const runtime = 'nodejs';
-
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 const GITHUB_SECRET = process.env.GITHUB_SECRET ?? '';
@@ -55,40 +55,13 @@ const verifySignature = async (req: Request) => {
   }
 };
 
-const onStar = (payload: {
-  action: string;
-  repository: { full_name: string };
-  sender: {
-    login: string;
-  };
-}): string => {
-  const { action, sender, repository } = payload;
-
-  return `User ${sender.login} ${action} star on ${repository.full_name}`;
-};
-
-const onIssue = (payload: {
-  action: string;
-  issue: { title: string; user: { login: string } };
-}): string => {
-  const { action, issue } = payload;
-
-  if (action === 'opened') {
-    return `An issue was opened with this title ${issue.title}`;
-  }
-
-  if (action === 'closed') {
-    return `An issue was closed by ${issue.user.login}`;
-  }
-
-  return `Unhandled action for the issue event ${action}`;
-};
-
 const handler = async (req: Request) => {
   try {
     const body = await req.json();
 
     const githubEvent = req.headers.get('x-github-event') ?? 'unknown';
+
+    console.log(req.headers);
 
     const isGithubReq = await verifySignature(req);
 
